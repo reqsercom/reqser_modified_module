fb350509f1263523729c004d2d2d6c85d63e98e4
JorisK <jorisklostermann@hotmail.com>
2023-11-12 19:15:00 +0100

    get_magic_quotes_gpc Bugfix for < 7.4

--- Shoproot/includes/external/api_local/classes/db/DbFuncs.php
+++ Shoproot/includes/external/api_local/classes/db/DbFuncs.php
@@ -101,7 +101,10 @@ class DbFuncs {
     }
 
     if(version_compare(PHP_VERSION, '7.4', '<')) {
-      $gmqu = get_magic_quotes_gpc();
+      //JorisK the get_magic_quotes_gpc is not always actiavated also before 7.4
+      if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
+        $gmqu = get_magic_quotes_gpc();
+      }
     }
 
     if(!empty($params)) {
