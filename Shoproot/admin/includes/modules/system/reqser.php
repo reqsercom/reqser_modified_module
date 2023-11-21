5ed4b045bba3bf504761874d234e958ec2d0bcdc
JorisK <jorisklostermann@hotmail.com>
2023-11-21 18:15:49 +0100

    update

--- Shoproot/admin/includes/modules/system/reqser.php
+++ Shoproot/admin/includes/modules/system/reqser.php
@@ -37,8 +37,10 @@ class reqser {
     $this->shop_version_lt_2060 = (defined('PROJECT_MAJOR_VERSION') && PROJECT_MAJOR_VERSION == '2' && defined('PROJECT_MINOR_VERSION') && str_replace('.', '', PROJECT_MINOR_VERSION) < str_replace('.', '', '0.6.0')) || !function_exists('xtc_cfg_multi_checkbox');
 
     //BOC check for new version, Joris, noRiddle, 11-2023
+    static $reqser_update_request = false;
+    static $reqser_error_message = '';
     $local_api_key = constant($this->mn_const.'REQSER_API_KEY');
-    if($local_api_key != '' && $this->check() !== false) { 
+    if($local_api_key != '' && $this->check() !== false && $reqser_update_request !== true) { // && $reqser_update_request !== true JorisK 21.11.2023 funtkioniert nicht weil er dann beim 2ten Aufruf den Titel nicht mehr ergänzt!
       require_once(DIR_FS_EXTERNAL.'api_local/classes/ApiBase.php');
       $api_base = new api_local\ApiBase();
       $url_credential = 'https://reqser.com/api/token';
@@ -50,8 +52,12 @@ class reqser {
         $result_request = $api_base->doRequest($url_requ, 'post', 'json', 'json', $post_fields, array('token' => $token_verify['access_token']), NULL, 'y', 5);
         if(isset($result_request['warning_message']) && $result_request['warning_message'] != ''){
           $this->title .= '<br><span style="color:red;">'.$result_request["warning_message"].'</span>';
+          $reqser_error_message = $result_request["warning_message"];
         }
       }
+      $reqser_update_request = true;
+    } elseif ($reqser_update_request === true && $reqser_error_message != '') {
+      $this->title .= '<br><span style="color:red;">'.$reqser_error_message.'</span>';
     }
     //EOC check for new version, Joris, noRiddle, 11-2023
 
