cd781bba564c05709a5ed56e856dceb309b25625
JorisK <jorisklostermann@hotmail.com>
2023-11-27 10:10:52 +0100

    CMS Version

--- Shoproot/admin/includes/modules/system/reqser.php
+++ Shoproot/admin/includes/modules/system/reqser.php
@@ -48,7 +48,7 @@ class reqser {
       $token_verify = $api_base->doRequest($url_credential, 'post', 'normal', 'json', $vals_credential, array('token' => $local_api_key), NULL, 'y', 5);
       if(!isset($token_verify['warning_message'])){
         $url_requ = 'https://reqser.com/api/module_request';
-        $post_fields = array('cms' => 'Modified','cms_major_version' => PROJECT_MAJOR_VERSION, 'cms_minor_version' => PROJECT_MINOR_VERSION, 'php_version' => phpversion(), 'module_version' => $this->module_version);
+        $post_fields = array('cms' => 'Modified','cms_version' => PROJECT_MAJOR_VERSION.'.'.PROJECT_MINOR_VERSION, 'php_version' => phpversion(), 'module_version' => $this->module_version);
         $result_request = $api_base->doRequest($url_requ, 'post', 'json', 'json', $post_fields, array('token' => $token_verify['access_token']), NULL, 'y', 5);
         if(isset($result_request['warning_message']) && $result_request['warning_message'] != ''){
           $this->title .= '<br><span style="color:red;">'.$result_request["warning_message"].'</span>';
