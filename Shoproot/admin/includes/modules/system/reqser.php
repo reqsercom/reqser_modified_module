a9c04161f3882988aee3684b2a1b124fcbc4e8fd
JorisK <jorisklostermann@hotmail.com>
2024-01-02 17:19:19 +0100

    New Installation needed

--- Shoproot/admin/includes/modules/system/reqser.php
+++ Shoproot/admin/includes/modules/system/reqser.php
@@ -36,6 +36,11 @@ class reqser {
       $this->title .= '<br><span style="color:red;">PHP Version is too low, min. 7.0 required!</span>';
     }
 
+    $new_installation_needed = false;
+    if ($this->mn_const.'INSTALLED_MODUL_VERSION' != '' && $this->mn_const.'INSTALLED_MODUL_VERSION' != $this->module_version){
+      $this->title .= '<br><span style="color:red;">Installed Modul Version not the same as uploaded, please reinstall this module!</span>';
+    }
+
     $this->langs_arr_str = get_langs_from_translate();
 
     if(isset($_GET['module']) && $_GET['module'] == $this->code && isset($_GET['action']) && $_GET['action'] == 'save') {
@@ -94,6 +99,7 @@ class reqser {
     xtc_db_query("INSERT INTO ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) VALUES ('".$this->mn_const."REQSER_API_KEY', '', '6', '2', now())");
     xtc_db_query("INSERT INTO ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) VALUES ('".$this->mn_const."TEMP_SHOP_TOKEN', '', '6', '4', now())");
     xtc_db_query("INSERT INTO ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) VALUES ('".$this->mn_const."TST_VALID_UNTIL', '', '6', '5', now())");
+    xtc_db_query("INSERT INTO ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, date_added) VALUES ('".$this->mn_const."INSTALLED_MODUL_VERSION', '".$this->module_version."', '6', '5', now())");
 
     xtc_db_query("INSERT INTO ".TABLE_CONFIGURATION." (configuration_key, configuration_value, configuration_group_id, sort_order, set_function, date_added) VALUES ('".$this->mn_const.'ALLOW_ALL_ROW_ACCESS'."', 'true', '6', '3', 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())");
     $default_tables = 'banners,categories_description,content_manager,content_manager_content,coupons_description,customers_status,email_content,manufacturers_info,orders_status,products_content,products_description,products_options,products_options_values,products_tags_options,products_tags_values,products_vpe,products_xsell_grp_name,shipping_status';
