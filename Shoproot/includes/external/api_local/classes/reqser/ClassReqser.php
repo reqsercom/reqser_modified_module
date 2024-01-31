d2151b7bd10cf53107768d5164d0fb0de7be878f
JorisK <jorisklostermann@hotmail.com>
2024-01-31 13:33:35 +0100

    First updates for 2.7

--- Shoproot/includes/external/api_local/classes/reqser/ClassReqser.php
+++ Shoproot/includes/external/api_local/classes/reqser/ClassReqser.php
@@ -36,7 +36,7 @@ class ClassReqser extends api_local\ApiBase {
   public function __construct($subp = '') {
     parent::__construct($subp);
 
-    $this->api_reqser_version = '2.6';
+    $this->api_reqser_version = '2.7';
     $this->browser_mode = false;
     $this->dev_mode = true;
     $this->write_control_mode = false;
