cc6cb687d28b85072c2c30e8c0c1ea765d683034
JorisK <jorisklostermann@hotmail.com>
2025-03-08 17:27:04 +0100

    Bugfix

--- Shoproot/includes/extra/modules/product_info_mo_images/reqser_remove_main_image_from_mo_images.php
+++ Shoproot/includes/extra/modules/product_info_mo_images/reqser_remove_main_image_from_mo_images.php
@@ -2,7 +2,7 @@
 
 if (defined('MODULE_SYSTEM_REQSER_STATUS') && constant('MODULE_SYSTEM_REQSER_STATUS') == 'true' && defined('MODULE_SYSTEM_REQSER_IMAGE_TAGS_ACTIVE') && constant('MODULE_SYSTEM_REQSER_IMAGE_TAGS_ACTIVE') == 'true') {
     if (isset($more_images_data[0])){
-        $info_smarty->assign('reqser_main_image_tag', $more_images_data);
+        $info_smarty->assign('reqser_main_image_tag', $more_images_data[0]);
         unset($more_images_data[0]);
     } 
 }
