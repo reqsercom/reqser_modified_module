a7e1bded1ed4b38d049883db29a73280e1ee3b45
JorisK <jorisklostermann@hotmail.com>
2025-03-08 18:23:48 +0100

    remove -1

--- Shoproot/includes/extra/modules/product_info_mo_images/reqser_remove_main_image_from_mo_images.php
+++ Shoproot/includes/extra/modules/product_info_mo_images/reqser_remove_main_image_from_mo_images.php
@@ -8,9 +8,9 @@
 ********************************************************************/
 
 if (defined('MODULE_SYSTEM_REQSER_STATUS') && constant('MODULE_SYSTEM_REQSER_STATUS') == 'true' && defined('MODULE_SYSTEM_REQSER_IMAGE_TAGS_ACTIVE') && constant('MODULE_SYSTEM_REQSER_IMAGE_TAGS_ACTIVE') == 'true') {
-    if (isset($more_images_data[0])){
+    if (isset($more_images_data[-1])){
         //Damit das wirksam wird muss iM Template {reqser_main_image_tag} an der richtigen Stelle eingefügt werden
         $info_smarty->assign('reqser_main_image_tag', $more_images_data[0]);
-        unset($more_images_data[0]);
+        unset($more_images_data[-1]);
     } 
 }
