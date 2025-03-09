323bf013f3ac0644742b4586ad30ab44df21929f
JorisK <jorisklostermann@hotmail.com>
2025-03-09 18:05:44 +0100

    remove block form

--- Shoproot/admin/includes/extra/modules/new_product/reqser_deepl_translator_products.php
+++ Shoproot/admin/includes/extra/modules/new_product/reqser_deepl_translator_products.php
@@ -75,7 +75,6 @@
               }
             });
             $('#new_product').submit(function(e) {
-              e.preventDefault();
               const msreq_params = {
                 ext: 'reqser_upd_qu_ajax', 
                 type: 'plain', 
@@ -85,8 +84,6 @@
               };
               msreq_params[msreq_tok_key] = msreq_tok_val;
               $.post("../ajax.php", msreq_params);
-              $('#new_product').off('submit');
-              $('#new_product').submit();
           });
           };
   
